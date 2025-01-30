96415e5b630d4f56061158a9b9bd5361b2a05326
Tim Haskins <tijohask@gmail.com>
2025-01-30 19:35:36 +0000

    adding banned video download

---
+++ banned-download.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+while getopts n: flag
+do
+    case "${flag}" in
+        n) number=${OPTARG};;
+    esac
+done
+
+#cp rum-files.txt files.txt
+
+if [ -n $number ] && [  $number -gt 0 ] 2>/dev/null; then
+  echo $number
+  head -n $number ban.txt | xargs yt-dlp -f "hls-0" -o "%(title).150B.%(ext)s" -x --audio-format mp3 --parse-metadata "%(channel)s:%(meta_artist)s" --embed-metadata
+  head -n $number ban.txt > backup.txt
+#  wc -l ban.txt | awk '{print $number-1}' | xargs tail ban.txt -n > out.txt
+  a=($(wc ban.txt))
+  lines=${a[0]}
+  echo $(($lines - $number))
+  tail ban.txt -n $(($lines - $number)) > out.txt
+  mv out.txt ban.txt
+else
+  yt-dlp --batch-file ban.txt -f "hls-group_audio-original" -o "%(title).150B.%(ext)s" -x --audio-format mp3 --parse-metadata "%(channel)s:%(meta_artist)s" --embed-metadata
+  mv ban.txt backup.txt
+  touch ban.txt
+fi
+
+mv *.mp3 /sdcard/Music/Longform/
+
