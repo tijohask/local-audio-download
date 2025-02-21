84f16f63f72d1b32f6009b30c0e7efc8fb7d836e
Tim Haskins <tijohask@gmail.com>
2025-02-21 16:47:40 +0000

    Fixing bug where certain vids recorded on twitter were not downloading, cleaning up unused code.

--- x-download.sh
+++ x-download.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-yt-dlp --batch-file x-files.txt -f "ba" -o "%(title).150B.%(ext)s" -x --audio-format mp3 --embed-metadata
-
-#for f in *.mp4
-#do
-#  ffmpeg -i "$f" "${f%.mp4}.mp3"
-#done
+yt-dlp --batch-file x-files.txt -f "ba/replay-300" -o "%(title).150B.%(ext)s" -x --audio-format mp3 --embed-metadata
 
 mv *.mp3 /sdcard/Music/Longform/
 
 mv x-files.txt backup.txt
 
 touch x-files.txt
-
-#rm *.mp4
